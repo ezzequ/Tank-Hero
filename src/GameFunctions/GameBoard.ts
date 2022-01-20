1fea74b365d514177f57ef500ec2a739784a63eb
Tomas Fridekrans <vindicated@hotmail.com>
2022-01-20 10:56:16 +0100

    Added human images to sidebar. Some text on gamecounter.
    Co-authored-by: Felix <FrontFelix@users.noreply.github.com>
    Co-authored-by: Kawium <Kawium@users.noreply.github.com>
    Co-authored-by: Esther <ezzequ@users.noreply.github.com>
    Co-authored-by: Christian Meiner <miMeiner@users.noreply.github.com>

--- src/GameFunctions/GameBoard.ts
+++ src/GameFunctions/GameBoard.ts
@@ -1,59 +1,54 @@
 class GameBoard {
-  private background: p5.Image;
-  private xPos: number;
-  private xPos2: number;
+  private background: p5.Image
+  private xPos: number
+  private xPos2: number
 
-
-  private scrollSpeed: number;
+  private scrollSpeed: number
   private gameCounter: GameCounter
-  private entities: Entity[];
-  private tank: Tank;
-  private zombie: Zombie;
-  private obstacle: Obstacle;
-  private human: Human;
+  private entities: Entity[]
+  private tank: Tank
+  private zombie: Zombie
+  private obstacle: Obstacle
+  private human: Human
   private sideBoard: SideBoard
 
   constructor() {
     this.gameCounter = new GameCounter()
-    this.sideBoard = new SideBoard();
-    this.tank = new Tank();
-    this.zombie = new Zombie();
-    this.obstacle = new Obstacle();
-    this.human = new Human();
-    this.entities = []
+    this.sideBoard = new SideBoard()
+    this.tank = new Tank()
+    this.zombie = new Zombie()
+    this.obstacle = new Obstacle()
+    this.human = new Human()
+    this.entities = [new Zombie(), new Obstacle(), new Human()]
 
-    this.background = images.bgImg;
-    this.xPos = 0;
-    this.xPos2 = width;
-    this.scrollSpeed = 5;
-
-  
+    this.background = images.bgImg
+    this.xPos = 0
+    this.xPos2 = width
+    this.scrollSpeed = 5
   }
 
   private scroll() {
     image(this.background, this.xPos, 0, width, height)
     image(this.background, this.xPos2, 0, width, height)
 
-    this.xPos -= this.scrollSpeed;
-    this.xPos2 -= this.scrollSpeed;
+    this.xPos -= this.scrollSpeed
+    this.xPos2 -= this.scrollSpeed
 
     if (this.xPos < -width) {
-      this.xPos = width;
+      this.xPos = width
     }
     if (this.xPos2 < -width) {
-      this.xPos2 = width;
+      this.xPos2 = width
     }
     loop()
   }
 
+  private spawnEntities() {
+    let rndNumber = random(this.entities.length)
+    rndNumber = Math.floor(rndNumber)
+    return this.entities[rndNumber]
+  }
 
-    private spawnEntities() {
-      this.entities.push(new Zombie());
-      this.entities.push(new Obstacle());
-      this.entities.push(new Human());     
-      // this.entities[0].draw()
-      
-    }
   //   private checkCollision() {
   //     // Return Void
   //   }
@@ -67,38 +62,26 @@ class GameBoard {
   //   }
   public update() {
     // Return Void
-    this.tank.move(); // RÖR TANKEN
-    this.zombie.update();
-    this.obstacle.update();
-    this.human.update();
-    this.tank.updateBullet();
-    this.tank.keyTyped();
-  }  
-
-  
-  
-  public draw() {
-    this.scroll();
-    this.tank.draw();
-    this.zombie.draw();
-    this.obstacle.draw();
-    this.human.draw();
-    this.sideBoard.draw();
-    this.gameCounter.draw();
-    this.tank.renderBullet();
-    
-    
-    
-      
-    
-    
-    //console.log(this.renderZombie())
-    }
-
-  
-
-
+    this.tank.move() // RÖR TANKEN
+    this.zombie.update()
+    this.obstacle.update()
+    this.human.update()
+    this.tank.updateBullet()
+    this.tank.keyTyped()
+    //this.spawnEntities().update()
   }
 
+  public draw() {
+    this.scroll()
+    this.tank.draw()
+    this.zombie.draw()
+    this.obstacle.draw()
+    this.human.draw()
+    this.sideBoard.draw()
+    this.gameCounter.draw()
+    this.tank.renderBullet()
+    //this.spawnEntities().draw()
 
-
+    //console.log(this.renderZombie())
+  }
+}
--- src/GameFunctions/Sideboard.ts
+++ src/GameFunctions/Sideboard.ts
@@ -1,7 +1,42 @@
 class SideBoard {
-  // private rescuedSurvivor : ? = ?
+  private human: p5.Image
+  private rescuedLives: p5.Image[]
+  private humanSize: number
+  private sideBoardHeight: number
+  private startingLives: number
 
-  constructor() {}
+  constructor() {
+    this.human = images.human
+    this.rescuedLives = [this.human, this.human, this.human, this.human]
+    this.humanSize = 100
+    this.sideBoardHeight = height / 6
+    this.startingLives = 4
+  }
+
+  /*private rescuedLives() {
+    let rescuedLives = [1,3,4,5]
+    //let rescuedLive: Array<number> = [1,3,4,5]
+    
+  }*/
+
+  public renderLives() {
+    let addNumber: number = 0
+    for (let live of this.rescuedLives) {
+      addNumber += 100
+      image(
+        live,
+        50,
+        this.sideBoardHeight + addNumber,
+        this.humanSize,
+        this.humanSize
+      )
+    }
+  }
+
+  public addLives() {
+    console.log('test')
+    console.log(this.rescuedLives.push(this.human))
+  }
 
   private deathEffect() {
     // Return Void
@@ -14,5 +49,6 @@ class SideBoard {
   public draw() {
     fill(204, 102, 0)
     rect(0, 0, windowWidth / 6, windowHeight)
+    this.renderLives()
   }
 }
